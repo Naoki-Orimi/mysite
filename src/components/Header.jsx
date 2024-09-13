8a6a9ac64cf5cbdfd0b110b4036bd524a49be2b1
Naoki-Orimi <n0a5o2k6i@gmail.com>
2024-09-13 21:14:46 +0900

    アニメーション追加 Headerの作成 途中

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { motion } from 'framer-motion';
-// import { ToggleButton } from '@mui/material';
+import { Button } from '@mui/material';
 import './header.scss';
 
 const Header = () => {
@@ -12,11 +12,24 @@ const Header = () => {
 		'Skills',
 	];
 
+	// ハンバーガーメニューの追加
+	const handleClickMenu = () => {
+		console.log('menuボタンが押されました');
+		if(isopen) {
+			console.log('setIsOpen = false に設定します');
+			setIsOpen(false);
+		} else {
+			console.log('setIsOpen = true に設定します');
+			setIsOpen(true);
+		}
+	};
+
 	return (
 		<div>
 			<motion.div className='header'>
 			<div className='header-contactus'>contact us</div>
-			<div className='header-menu'>menus</div>		
+			{/* <input className='header-menu' type='button' value='menus' onClick={handleClickMenu} /> */}
+			<input className='header-menu' value='menus' type='button' onClick={handleClickMenu} />
 			</motion.div>
 		</div>
 	);
