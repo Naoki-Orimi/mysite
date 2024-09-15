7195d22cdac9e0a82b9f7caac561c1cf61fcc88f
Naoki-Orimi <n0a5o2k6i@gmail.com>
2024-09-15 18:25:46 +0900

    ヘッダモーダル作成中

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { motion } from 'framer-motion';
-import { Button } from '@mui/material';
 import './header.scss';
 
 const Header = () => {
@@ -15,24 +14,59 @@ const Header = () => {
 	// ハンバーガーメニューの追加
 	const handleClickMenu = () => {
 		console.log('menuボタンが押されました');
-		if(isopen) {
-			console.log('setIsOpen = false に設定します');
-			setIsOpen(false);
-		} else {
-			console.log('setIsOpen = true に設定します');
+		if (!isopen) {
+			console.log('メニュー開く');
 			setIsOpen(true);
+		} else {
+			console.log('メニュー閉じる');
+			setIsOpen(false);
 		}
 	};
 
 	return (
-		<div>
-			<motion.div className='header'>
-			<div className='header-contactus'>contact us</div>
-			{/* <input className='header-menu' type='button' value='menus' onClick={handleClickMenu} /> */}
-			<input className='header-menu' value='menus' type='button' onClick={handleClickMenu} />
-			</motion.div>
-		</div>
-	);
+    <div>
+        <motion.div
+          className='header'
+          initial={{ opacity: 1 }} // 初期状態でボタンは見える
+          exit={{ opacity: 0 }} // 要素が消える時の透明度を0にしてフェードアウトさせているが、そもそもボタン押下時にHeaderを非表示にしているため意味ない
+        >
+          <div className='header-contactus'>Contact Us</div>
+          <button
+            className='header-menu'
+            onClick={handleClickMenu}
+          >
+            Menu
+          </button>
+					{/* メニューモーダル */}
+					{isopen && (
+						<motion.div
+							className='header-menu-list'
+							initial={{ opacity: 0, y: -100 }} // 初期状態（上からフェードイン）
+							animate={{ opacity: 1, y: 0 }}   // フェードインして中央にスライド
+							exit={{ opacity: 0, y: 100 }}     // フェードアウトして下にスライド
+							transition={{ duration: 0.5 }}    // アニメーションの速さを指定
+						>
+							{/* <ul style={{ listStyle: 'none', padding: 50 }}> */}
+							<ul>
+								{menuList.map((menuItem, index) => (
+									<li key={index} style={{ marginBottom: '10px', fontSize: '20px' }}>
+										{menuItem}
+									</li>
+								))}
+									<li>
+										<button 
+											onClick={handleClickMenu}
+										>
+											Back
+										</button>
+									</li>
+							</ul>
+						</motion.div>
+					)}
+        </motion.div>
+
+    </div>
+  );
 }
 
 export default Header;
