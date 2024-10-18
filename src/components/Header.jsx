a3d9f7c37f312da446efa9b65300661e1de35084
Naoki-Orimi <n0a5o2k6i@gmail.com>
2024-10-18 14:11:53 +0900

    Header css→tailwindに変更

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
-import './header.scss';
 
 const Header = () => {
 	// メニューの状態
@@ -24,48 +23,50 @@ const Header = () => {
 	};
 
 	return (
-		<div>
-			<motion.div
-				className='header'
-				initial={{ opacity: 1 }}
+	<div>
+		<div className='header flex justify-between items-center px-10 py-5' initial={{ opacity: 1 }}>
+			<div className='
+				sm:text-[10px] md:text-[30px]
+				transform transition-transform hover:scale-110 duration-500
+			'>
+				Contact Us
+			</div>
+			<button 
+				className='
+					sm:text-[10px] md:text-[30px]
+					transform transition-transform hover:scale-110 duration-500'
+				onClick={handleClickMenu}
 			>
-				<div className='header-contactus'>Contact Us</div>
-				<button
-					className='header-menu'
-					onClick={handleClickMenu}
-				>
-					Menu
-				</button>
-				{/* メニューモーダル AnimatePresenceで、motion.divのexitアニメーションを適用させる*/}
-				<AnimatePresence>
-					{isopen && (
-						<motion.div
-							className='header-menu-contents'
-							initial={{ opacity: 0, y: 0 }}   // 初期状態（ふわっと現れるように）
-							animate={{ opacity: 1, y: 0 }}   // フェードインして中央にスライド
-							transition={{ duration: 0.5 }}   // アニメーションの速さを指定
-							exit={{ opacity: 0, y: 100 }}    // Animateフェードアウトして下にスライド
-						>
-							<ul>
-								{menuList.map((menuItem, index) => (
-									<li key={index} style={{ marginBottom: '10px', fontSize: '20px' }}>
-										{menuItem}
-									</li>
-								))}
-								<li>
-									<motion.button
-										className='header-menu-contents-back'
-										onClick={handleClickMenu}
-									>
-										Back
-									</motion.button>
-								</li>
-							</ul>
-						</motion.div>
-					)}
-				</AnimatePresence>
-			</motion.div>
+				Menu
+			</button>
 		</div>
+	
+		{/* メニューモーダル AnimatePresenceで、motion.divのexitアニメーションを適用させる */}
+		<AnimatePresence>
+			{isopen && (
+				<motion.div
+					className='header-menu-contents'
+					initial={{ opacity: 0, y: 0 }}   // 初期状態（ふわっと現れるように）
+					animate={{ opacity: 1, y: 0 }}   // フェードインして中央にスライド
+					transition={{ duration: 0.5 }}   // アニメーションの速さを指定
+					exit={{ opacity: 0, y: 100 }}    // Animateフェードアウトして下にスライド
+				>
+					<ul className='menu-list'>
+						{menuList.map((menuItem, index) => (
+							<li key={index} className='mb-4 text-[20px]'>
+								{menuItem}
+							</li>
+						))}
+						<li>
+							<motion.button className='header-menu-contents-back' onClick={handleClickMenu}>
+								Back
+							</motion.button>
+						</li>
+					</ul>
+				</motion.div>
+			)}
+		</AnimatePresence>
+	</div>
 	);
 }
 
