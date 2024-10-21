f6bbc0f2b005d8f6ebbb0a5d23dc59affae8374c
Naoki-Orimi <n0a5o2k6i@gmail.com>
2024-10-21 09:35:04 +0900

    TOP画像実装

--- src/components/contents/Top.jsx
+++ src/components/contents/Top.jsx
@@ -3,7 +3,6 @@ import { motion } from 'framer-motion';
 import './top.scss';
 
 const Top = () => {
-  const topImgRelativePath = "facet/img/sea-ocean.jpg";
   // TODO： useStateとuseEffectを使用して、画像スライドアニメーションを作成したい
   const imagePathList = [
     'facet/img/sea-ocean.jpg',
@@ -12,21 +11,35 @@ const Top = () => {
   ];
 
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [topImgRelativePath, setTopImgRelativePath] = useState(imagePathList[0]);
 
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentImageIndex((prevIndex) =>
         prevIndex === imagePathList.length - 1 ? 0 : prevIndex + 1
       );
-    }, 3000); // 3秒ごとに画像を切り替える
+      // console.log("hoge");
+    }, 5000); // 5秒ごとに画像を切り替える
     return () => clearInterval(interval);
   }, [imagePathList.length]);
+
+  useEffect(() => {
+    setTopImgRelativePath(imagePathList[currentImageIndex]);
+  }, [currentImageIndex, imagePathList]);
 
   return (
     <div>
       {/* 上部画像部分 */}
       <motion.div className="top-img">
-        <img src={topImgRelativePath} alt="ocean" />
+        <motion.img
+          key={topImgRelativePath} // keyを追加してアニメーションを再適用
+          src={topImgRelativePath}
+          alt="superBanner"
+          initial={{ opacity: 0 }} // フェードアウトの初期状態
+          animate={{ opacity: 1 }} // フェードイン
+          exit={{ opacity: 0 }} // フェードアウト
+          transition={{ duration: 3 }} // アニメーションの速さを設定
+        />
         <motion.p
           className='top-img-text'
           initial={{ opacity: 0, x: 0 }}
@@ -36,9 +49,9 @@ const Top = () => {
           Urakata Enginner
         </motion.p>
       </motion.div>
-      <motion.div className="top-contents">
-        <div>show any contents</div>
-      </motion.div>
+      <div className="top-contents px-10 py-5">
+        <div>show any contents. comming soon</div>
+      </div>
     </div>
   );
 }
